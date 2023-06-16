efc32bd72d2016d06ca69c12dbbdbf08c94ca26b
premKumarsingh <premKumarsingh@DESKTOP-CMSF7SI>
2023-06-16 19:45:12 +0530

    Add to git repo

---
+++ Test Suites/PREM NEW SUITE.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>PREM NEW SUITE</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>9938fe0f-d3a7-4011-8d2d-000654cab328</testSuiteGuid>
+   <testCaseLink>
+      <guid>8be04445-ae2c-4f39-9387-3f7e00e04228</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/New Test Case</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
