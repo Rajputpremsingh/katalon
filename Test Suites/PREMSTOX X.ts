21a9b83e22dd66227f41793def45b15a3e5f8e29
premKumarsingh <premKumarsingh@DESKTOP-CMSF7SI>
2023-06-16 22:27:26 +0530

    Add to repo

---
+++ Test Suites/PREMSTOX X.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>PREMSTOX X</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>c92d742b-d4c5-4b75-ac2b-1ec98d157dbd</testSuiteGuid>
+   <testCaseLink>
+      <guid>e60a108d-3784-44d5-84e0-1e1e381aa092</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/PREM NEW</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
