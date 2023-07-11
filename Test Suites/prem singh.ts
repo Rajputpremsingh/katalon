5cb5ba25b849adc6e4e4bcff6bef2cf64337dfbd
premKumarsingh <premKumarsingh@DESKTOP-CMSF7SI>
2023-07-11 20:33:54 +0530

    add pro

---
+++ Test Suites/prem singh.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>prem singh</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>85e603f3-15a8-4d78-b3eb-f0235ffc28a9</testSuiteGuid>
+   <testCaseLink>
+      <guid>2374b33b-d2bf-4510-be5b-2e1244bd9e90</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/gb</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
